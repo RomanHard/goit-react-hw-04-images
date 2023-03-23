0fbcb90e1461b6be31cb0c71ab18c38b69c0390c
Roman <romeenice@gmail.com>
2023-03-23 23:43:58 +0200

    ImageGallery

--- src/components/imageGallery/ImageGallery.js
+++ src/components/imageGallery/ImageGallery.js
@@ -1,23 +1,18 @@
-import React, { Component } from 'react';
+
+import React from 'react';
 import './ImageGallery-module.css';
 import ImageGalleryItem from '../imageGalleryItem/ImageGalleryItem';
 
-class ImageGallery extends Component {
-  render() {
-    const { images, onClick } = this.props;
-
-    return (
-      <ul className="gallery">
-        {images.map((image, index) => (
-          <ImageGalleryItem
-            key={`${image.webformatURL}-${index}`}
-            webformatURL={image.webformatURL}
-            onClick={() => onClick(image.largeImageURL)}
-          />
-        ))}
-      </ul>
-    );
-  }
+export default function ImageGallery({ images, onClick }) {
+  return (
+    <ul className="gallery">
+      {images.map((image, index) => (
+        <ImageGalleryItem
+          key={`${image.webformatURL}-${index}`}
+          webformatURL={image.webformatURL}
+          onClick={() => onClick(image.largeImageURL)}
+        />
+      ))}
+    </ul>
+  );
 }
-
-export default ImageGallery;
